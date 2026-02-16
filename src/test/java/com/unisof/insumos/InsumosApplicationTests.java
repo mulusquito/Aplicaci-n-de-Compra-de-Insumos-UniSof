b769d89f1690830c032cbccfa38032d38a9ece98
alfonsoocampo999 <141872515+alfonsoocampo999@users.noreply.github.com>
2026-02-15 20:38:39 -0500

    inicio del proyecto y conexion a base de datos

---
+++ src/test/java/com/unisof/insumos/InsumosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisof.insumos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsumosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
